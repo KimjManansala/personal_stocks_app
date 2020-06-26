e0300ff27b2b380c799cf0deeca5fa8be92ad84d
KimjManansala <kimjmanansala@gmail.com>
2020-06-25 21:19:00 -0400

    Created first page with react router

--- client/App.js
+++ client/App.js
@@ -1,11 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+
+import { NativeRouter, Route, Link } from "react-router-native";
+
+const Home = () => <Text style={styles.header}>Home</Text>;
+
+const About = () => <Text style={styles.header}>About</Text>;
+
+const Topic = ({ match }) => (
+    <Text style={styles.topic}>{match.params.topicId}</Text>
+);
+
+const Topics = ({ match }) => (
+    <View>
+        <Text style={styles.header}>Topics</Text>
+        <View>
+            <Link
+                to={`${match.url}/rendering`}
+                style={styles.subNavItem}
+                underlayColor="#f0f4f7"
+            >
+            <Text>Rendering with React</Text>
+            </Link>
+            <Link
+                to={`${match.url}/components`}
+                style={styles.subNavItem}
+                underlayColor="#f0f4f7"
+            >
+            <Text>Components</Text>
+            </Link>
+            <Link
+                to={`${match.url}/props-v-state`}
+                style={styles.subNavItem}
+                underlayColor="#f0f4f7"
+            >
+            <Text>Props v. State</Text>
+            </Link>
+        </View>
+    
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+        <Route
+            exact
+            path={match.path}
+            render={() => (
+                <Text style={styles.topic}>Please select a topic.</Text>
+            )}
+        />
+    </View>
+);
 
 export default function App() {
     return (
-        <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <NativeRouter>
+            <View style={styles.container}>
+            <View style={styles.nav}>
+                <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+                <Text>Home</Text>
+                </Link>
+                <Link
+                to="/about"
+                underlayColor="#f0f4f7"
+                style={styles.navItem}
+                >
+                <Text>About</Text>
+                </Link>
+                <Link
+                to="/topics"
+                underlayColor="#f0f4f7"
+                style={styles.navItem}
+                >
+                <Text>Topics</Text>
+                </Link>
+            </View>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+            </View>
+        </NativeRouter>
     );
 }
 
